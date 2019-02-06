1d469bc2b3bf9f533f1a2963e4a69bb93121c52a
llunda <47144613+llunda@users.noreply.github.com>
2019-02-06 15:38:41 +0100

    Add files via upload

--- GLP_URBAIN/GLP_URBAIN_2019/src/ville/PlanVille.java
+++ GLP_URBAIN/GLP_URBAIN_2019/src/ville/PlanVille.java
@@ -2,13 +2,69 @@ package ville;
 
 import java.util.HashMap;
 
+
 public class PlanVille {
+	private int[][] plan;
+	private int i;
+	private int j;
 	private HashMap<String,BatimentCible> batiments;
 	private HashMap<String,Route> routes;
 	
-	public PlanVille() {
+	public PlanVille(int i, int j) {
+		this.i = i;
+		this.j = j;
+		plan = new int [25][25];
+		
+		for(i = 0; i< plan.length; i++) {
+			for(j=0; i< plan.length; j++) {
+				plan[i][j] = 0;
+				System.out.println(plan[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
 		
 	}
+	
+	
+
+	public int[][] getPlan() {
+		return plan;
+	}
+
+
+
+	public void setPlan(int[][] plan) {
+		this.plan = plan;
+	}
+
+
+
+	public int getI() {
+		return i;
+	}
+
+
+
+	public void setI(int i) {
+		this.i = i;
+	}
+
+
+
+	public int getY() {
+		return j;
+	}
+
+
+
+	public void setY(int j) {
+		this.j = j;
+	}
+	
+
+
+
 
 	public HashMap<String, BatimentCible> getBatiments() {
 		return batiments;
@@ -25,6 +81,5 @@ public class PlanVille {
 	public void setRoutes(HashMap<String, Route> routes) {
 		this.routes = routes;
 	}
-	
-	
+
 }
