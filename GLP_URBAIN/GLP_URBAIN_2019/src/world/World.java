358be8de9079eab7b3fb124308914533175c1a4a
QuentinLiguori <38524351+QuentinLiguori@users.noreply.github.com>
2019-02-19 22:14:44 +0100

    Add files via upload

--- GLP_URBAIN/GLP_URBAIN_2019/src/world/World.java
+++ GLP_URBAIN/GLP_URBAIN_2019/src/world/World.java
@@ -14,9 +14,9 @@ public class World {
 	}
 	public World() {
 		
-		Personnage jeanDuche = new Personnage("Duche","Jean", "masculin",54);
-		Personnage robertPate = new Personnage("Pate", "Robert", "masculin", 45);
-		Personnage christianeDupont = new Personnage("Dupont", "Christiane", "feminin", 39);
+		Personnage jeanDuche = new Personnage("Duche","Jean", "masculin",54,367,247,189,91);
+		Personnage robertPate = new Personnage("Pate", "Robert", "masculin", 45,12,34,467,236);
+		Personnage christianeDupont = new Personnage("Dupont", "Christiane", "feminin", 39, 409, 394,158,100);
 		
 		allCitizens = new ArrayList<>();
 		allCitizens.add(jeanDuche);
