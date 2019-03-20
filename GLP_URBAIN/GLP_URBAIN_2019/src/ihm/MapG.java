82f7eec8a87fcd70be5e7f0665d2b16120b20ca2
llunda <47144613+llunda@users.noreply.github.com>
2019-03-20 13:55:47 +0100

    Add files via upload

--- GLP_URBAIN/GLP_URBAIN_2019/src/ihm/MapG.java
+++ GLP_URBAIN/GLP_URBAIN_2019/src/ihm/MapG.java
@@ -10,7 +10,19 @@ import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
+import perso.Personnage;
+import ville.PlanVille;
+import world.World;
+
 public class MapG extends JPanel {
+	
+	private PlanVille ville;
+	private World world;
+	private PersoCreation creation;
+	
+	private Image image0;
+	private Image image00;
+	private Image image000;
 	private Image image1;
 	private Image image2;
 	private Image image3;
@@ -39,14 +51,18 @@ public class MapG extends JPanel {
 
 	public MapG() {
 		mapPane = new JPanel();
+		world = new World();
+		ville = new PlanVille();
+		creation = new PersoCreation();
 		this.setBackground(Color.DARK_GRAY);
-		
-
 		
 	}
 	public void paintComponent(Graphics g) {
 		
 		try {
+			this.image0 = ImageIO.read(new File("pictures/tile_0051.jpg"));
+			this.image00 = ImageIO.read(new File("pictures/tile_0213.jpg"));
+			this.image000 = ImageIO.read(new File("pictures/tile_0375.jpg"));
 			this.image1 = ImageIO.read(new File("pictures/bar.jpg"));
 			this.image2 = ImageIO.read(new File("pictures/cine.jpg"));
 			this.image3 = ImageIO.read(new File("pictures/disco.jpg"));
@@ -69,38 +85,46 @@ public class MapG extends JPanel {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-//*******************Initialisation de la map**************************************************//
+			
+
 		super.paintComponent(g);
 		g.setColor(Color.WHITE);
-	
 		
-		g.drawImage(image17,0, 0, 252, 148,this);         //     1     //
-		g.drawImage(image9,331, 0, 168, 148,this);         //    2      //
-		g.drawImage(image11,509, 0, 168, 148,this);         //     3     //
-		g.drawImage(image16,756, 0, 420, 148,this);         //     4     //
-		g.drawImage(image12,1260, 0, 336, 409,this);         //     5     //
-		g.drawImage(image2,0, 222, 336, 227,this);         //       6   //
-		g.drawImage(image15,415, 222, 168, 148,this);         //      7    //
-		g.drawImage(image1,593, 222, 168, 148,this);         //       8   //
-		g.drawImage(image4,835, 222, 84, 148,this);         //    9      //
-		g.drawImage(image3,929, 222, 252, 148,this);         //    10      //
-		g.drawImage(image7,415, 380, 168, 148,this);         //      11    //
-		g.drawImage(image13,593, 380, 168, 148,this);         //   12       //
-		g.drawImage(image8,835, 380, 341, 148,this);         //     13     //
-		g.drawImage(image13,1420, 419, 168, 237,this);         //      14    //
-		g.drawImage(image10,0, 523, 168, 79,this);         //    15      //
-		g.drawImage(image5,247, 523, 84, 79,this);         //     16     //
-		g.drawImage(image1,1186, 489, 168, 158,this);         //    17      //
-		g.drawImage(image11,0, 612, 168, 138,this);         //      18    //
-		g.drawImage(image9,247, 612, 168, 138,this);         //      19    //
-		g.drawImage(image10,425, 612, 168, 138,this);         //    20     //
-		g.drawImage(image6,672, 612, 420, 281,this);         //       21   //
-		g.drawImage(image9,0, 760, 168, 133,this);         //      22    //
-		g.drawImage(image11,247, 760, 168, 133,this);         //      23    //
-		g.drawImage(image10,425, 760, 168, 133,this);         //      24    //
-		g.drawImage(image14,1176, 735, 420, 158,this);         //    25      //
+		//************************personnage*******************************************************//
+		for(int i = 0; i < creation.getWorld().getAllCitizens().size() ; i++) {
+            
+			g.drawImage(image0,creation.getWorld().getAllCitizens().get(i).getPositionX(),creation.getWorld().getAllCitizens().get(i).getPositionY(), 20, 20,this);
+        }
 		
+		//*******************Création Batiments**************************************************//
+		g.drawImage(image17,ville.getVilla().getHautGauche().getX(),ville.getVilla().getHautGauche().getY(),ville.getVilla().getBasDroite().getX()-ville.getVilla().getHautGauche().getX(),ville.getVilla().getBasDroite().getY()-ville.getVilla().getHautGauche().getY(),this);         //     villa     //
+		g.drawImage(image9,ville.getHome1().getHautGauche().getX(),ville.getHome1().getHautGauche().getY(),ville.getHome1().getBasDroite().getX()-ville.getHome1().getHautGauche().getX(),ville.getHome1().getBasDroite().getY()-ville.getHome1().getHautGauche().getY(),this);         //    home1      //
+		g.drawImage(image11,ville.getHome2().getHautGauche().getX(),ville.getHome2().getHautGauche().getY(),ville.getHome2().getBasDroite().getX()-ville.getHome2().getHautGauche().getX(),ville.getHome2().getBasDroite().getY()-ville.getHome2().getHautGauche().getY(),this);         //    home2    //
+		g.drawImage(image16,ville.getBureau1().getHautGauche().getX(),ville.getBureau1().getHautGauche().getY(),ville.getBureau1().getBasDroite().getX()-ville.getBureau1().getHautGauche().getX(),ville.getBureau1().getBasDroite().getY()-ville.getBureau1().getHautGauche().getY(),this);         //     bureau1     //
+		g.drawImage(image12,ville.getParc().getHautGauche().getX(),ville.getParc().getHautGauche().getY(),ville.getParc().getBasDroite().getX()-ville.getParc().getHautGauche().getX(),ville.getParc().getBasDroite().getY()-ville.getParc().getHautGauche().getY(),this);         //     parc     //
+		g.drawImage(image2,ville.getCinema().getHautGauche().getX(),ville.getCinema().getHautGauche().getY(),ville.getCinema().getBasDroite().getX()-ville.getCinema().getHautGauche().getX(),ville.getCinema().getBasDroite().getY()-ville.getCinema().getHautGauche().getY(),this);         //       cine//
+		g.drawImage(image15,ville.getBureau2().getHautGauche().getX(),ville.getBureau2().getHautGauche().getY(),ville.getBureau2().getBasDroite().getX()-ville.getBureau2().getHautGauche().getX(),ville.getBureau2().getBasDroite().getY()-ville.getBureau2().getHautGauche().getY(),this);         //    bureau2   //
+		g.drawImage(image1,ville.getBar1().getHautGauche().getX(),ville.getBar1().getHautGauche().getY(),ville.getBar1().getBasDroite().getX()-ville.getBar1().getHautGauche().getX(),ville.getBar1().getBasDroite().getY()-ville.getBar1().getHautGauche().getY(),this);         //       bar   //
+		g.drawImage(image4,ville.getFastFood1().getHautGauche().getX(),ville.getFastFood1().getHautGauche().getY(),ville.getFastFood1().getBasDroite().getX()-ville.getFastFood1().getHautGauche().getX(),ville.getFastFood1().getBasDroite().getY()-ville.getFastFood1().getHautGauche().getY(),this);         //    fastfood      //
+		g.drawImage(image3,ville.getDisco().getHautGauche().getX(),ville.getDisco().getHautGauche().getY(),ville.getDisco().getBasDroite().getX()-ville.getDisco().getHautGauche().getX(),ville.getDisco().getBasDroite().getY()-ville.getDisco().getHautGauche().getY(),this);         //    disco      //
+		g.drawImage(image7,ville.getParc2().getHautGauche().getX(),ville.getParc2().getHautGauche().getY(),ville.getParc2().getBasDroite().getX()-ville.getParc2().getHautGauche().getX(),ville.getParc2().getBasDroite().getY()-ville.getParc2().getHautGauche().getY(),this);         //      parc2    //
+		g.drawImage(image13,ville.getResto1().getHautGauche().getX(),ville.getResto1().getHautGauche().getY(),ville.getResto1().getBasDroite().getX()-ville.getResto1().getHautGauche().getX(),ville.getResto1().getBasDroite().getY()-ville.getResto1().getHautGauche().getY(),this);         //   resto1      //
+		g.drawImage(image8,ville.getMairie().getHautGauche().getX(),ville.getMairie().getHautGauche().getY(),ville.getMairie().getBasDroite().getX()-ville.getMairie().getHautGauche().getX(),ville.getMairie().getBasDroite().getY()-ville.getMairie().getHautGauche().getY(),this);         //     mairie     //
+		g.drawImage(image13,ville.getResto2().getHautGauche().getX(),ville.getResto2().getHautGauche().getY(),ville.getResto2().getBasDroite().getX()-ville.getResto2().getHautGauche().getX(),ville.getResto2().getBasDroite().getY()-ville.getResto2().getHautGauche().getY(),this);         //      resto2   //
+		g.drawImage(image10,ville.getHome3().getHautGauche().getX(),ville.getHome3().getHautGauche().getY(),ville.getHome3().getBasDroite().getX()-ville.getHome3().getHautGauche().getX(),ville.getHome3().getBasDroite().getY()-ville.getHome3().getHautGauche().getY(),this);         //    home3     //
+		g.drawImage(image5,ville.getFastFood2().getHautGauche().getX(),ville.getFastFood2().getHautGauche().getY(),ville.getFastFood2().getBasDroite().getX()-ville.getFastFood2().getHautGauche().getX(),ville.getFastFood2().getBasDroite().getY()-ville.getFastFood2().getHautGauche().getY(),this);         //    fastfood2     //
+		g.drawImage(image1,ville.getBar2().getHautGauche().getX(),ville.getBar2().getHautGauche().getY(),ville.getBar2().getBasDroite().getX()-ville.getBar2().getHautGauche().getX(),ville.getBar2().getBasDroite().getY()-ville.getBar2().getHautGauche().getY(),this);         //    bar2      //
+		g.drawImage(image11,ville.getHome4().getHautGauche().getX(),ville.getHome4().getHautGauche().getY(),ville.getHome4().getBasDroite().getX()-ville.getHome4().getHautGauche().getX(),ville.getHome4().getBasDroite().getY()-ville.getHome4().getHautGauche().getY(),this);         //      home4    //
+		g.drawImage(image9,ville.getHome5().getHautGauche().getX(),ville.getHome5().getHautGauche().getY(),ville.getHome5().getBasDroite().getX()-ville.getHome5().getHautGauche().getX(),ville.getHome5().getBasDroite().getY()-ville.getHome5().getHautGauche().getY(),this);         //      home5    //
+		g.drawImage(image10,ville.getHome6().getHautGauche().getX(),ville.getHome6().getHautGauche().getY(),ville.getHome6().getBasDroite().getX()-ville.getHome6().getHautGauche().getX(),ville.getHome6().getBasDroite().getY()-ville.getHome6().getHautGauche().getY(),this);         //    home6     //
+		g.drawImage(image6,ville.getHotel().getHautGauche().getX(),ville.getHotel().getHautGauche().getY(),ville.getHotel().getBasDroite().getX()-ville.getHotel().getHautGauche().getX(),ville.getHotel().getBasDroite().getY()-ville.getHotel().getHautGauche().getY(),this);         //       hotel   //
+		g.drawImage(image9,ville.getHome7().getHautGauche().getX(),ville.getHome7().getHautGauche().getY(),ville.getHome7().getBasDroite().getX()-ville.getHome7().getHautGauche().getX(),ville.getHome7().getBasDroite().getY()-ville.getHome7().getHautGauche().getY(),this);         //      home7    //
+		g.drawImage(image11,ville.getHome8().getHautGauche().getX(),ville.getHome8().getHautGauche().getY(),ville.getHome8().getBasDroite().getX()-ville.getHome8().getHautGauche().getX(),ville.getHome8().getBasDroite().getY()-ville.getHome8().getHautGauche().getY(),this);         //      home8    //
+		g.drawImage(image10,ville.getHome9().getHautGauche().getX(),ville.getHome9().getHautGauche().getY(),ville.getHome9().getBasDroite().getX()-ville.getHome9().getHautGauche().getX(),ville.getHome9().getBasDroite().getY()-ville.getHome9().getHautGauche().getY(),this);         //      home9    //
+		g.drawImage(image14,ville.getTheatre().getHautGauche().getX(),ville.getTheatre().getHautGauche().getY(),ville.getTheatre().getBasDroite().getX()-ville.getTheatre().getHautGauche().getX(),ville.getTheatre().getBasDroite().getY()-ville.getTheatre().getHautGauche().getY(),this);         //    theatre     //
+   
+		
+//*********************************dessins des routes*************************************************************//
 		
 	}
 	
