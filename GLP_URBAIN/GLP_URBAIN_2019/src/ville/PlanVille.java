118a0877c395476dfc5c9d164a34efb87786b6c5
msabia <47143643+msabia@users.noreply.github.com>
2019-04-10 14:23:48 +0200

    Add files via upload

--- GLP_URBAIN/GLP_URBAIN_2019/src/ville/PlanVille.java
+++ GLP_URBAIN/GLP_URBAIN_2019/src/ville/PlanVille.java
@@ -2,7 +2,11 @@ package ville;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import donnee.Point;
+import java.util.NoSuchElementException;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
+
+import data.Point;
 
 public class PlanVille {
 	private ArrayList<BatimentCible> batiments;
@@ -90,55 +94,55 @@ public class PlanVille {
 //******************Creation des batiments****************************************//
 		hgHome1 = new Point(331,0);
 		bdHome1 = new Point(499,148);
-		Home1 = new BatimentCible("Home1","Habitation",1,4,5,23,hgHome1,bdHome1,350,311,74);
+		Home1 = new BatimentCible("Home1","Energie",1,4,5,23,hgHome1,bdHome1,500,311,74);
 		batiments.add(Home1);
 		hgHome2 = new Point(509,0);
 		bdHome2 = new Point(677,148);
-		home2 = new BatimentCible("Home2","Habitation",1,4,5,23,hgHome2,bdHome2,350,677,74);
+		home2 = new BatimentCible("Home2","Energie",1,4,5,23,hgHome2,bdHome2,350,677,74);
 		batiments.add(home2);
 		hgHome3 = new Point(0,523);
 		bdHome3 = new Point(168,602);
-		home3 = new BatimentCible("Home3","Habitation",1,4,5,23,hgHome3,bdHome3,350,84,503);
+		home3 = new BatimentCible("Home3","Energie",1,4,5,23,hgHome3,bdHome3,350,84,503);
 		batiments.add(home3);
 		hgHome4 = new Point(0,612);
 		bdHome4 = new Point(168,750);
-		Home4 = new BatimentCible("Home4","Habitation",1,4,5,23,hgHome4,bdHome4,350,168,676);
+		Home4 = new BatimentCible("Home4","Energie",1,4,5,23,hgHome4,bdHome4,350,168,676);
 		batiments.add(Home4);
 		hgHome5 = new Point(247,612);
 		bdHome5 = new Point(415,750);
-		Home5 = new BatimentCible("Home5","Habitation",1,4,5,23,hgHome5,bdHome5,350,227,676);
+		Home5 = new BatimentCible("Home5","Energie",1,4,5,23,hgHome5,bdHome5,350,227,676);
 		batiments.add(Home5);
 		hgHome6 = new Point(425,612);
 		bdHome6 = new Point(593,750);
-		Home6 = new BatimentCible("Home6","Habitation",1,4,5,23,hgHome6,bdHome6,350,593,676);
+		Home6 = new BatimentCible("Home6","Energie",1,4,5,23,hgHome6,bdHome6,350,593,676);
 		batiments.add(Home6);
 		hgHome7 = new Point(0,760);
 		bdHome7 = new Point(168,893);
-		Home7 = new BatimentCible("Home7","Habitation",1,4,5,23,hgHome7,bdHome7,350,168,823);
+		Home7 = new BatimentCible("Home7","Energie",1,4,5,23,hgHome7,bdHome7,350,168,823);
 		batiments.add(Home7);
 		hgHome8 = new Point(247,760);
 		bdHome8 = new Point(415,893);
-		Home8 = new BatimentCible("Home8","Habitation",1,4,5,23,hgHome8,bdHome8,350,227,823);
+		Home8 = new BatimentCible("Home8","Energie",1,4,5,23,hgHome8,bdHome8,350,227,823);
 		batiments.add(Home8);
 		hgHome9 = new Point(425,760);
 		bdHome9 = new Point(593,893);
-		Home9 = new BatimentCible("Home9","Habitation",1,4,5,23,hgHome9,bdHome9,350,593,823);
+		Home9 = new BatimentCible("Home9","Energie",1,4,5,23,hgHome9,bdHome9,350,593,823);
 		batiments.add(Home9);
 		hgResto1 = new Point(593,380);
 		bdResto1 = new Point(761,528);
-		resto1 = new BatimentCible("RestoChinoi","Manger",0,50,11,23,hgResto1,bdResto1,300,680,528);
+		resto1 = new BatimentCible("RestoChinoi","Faim",0,50,11,23,hgResto1,bdResto1,500,680,528);
 		batiments.add(resto1);
 		hgResto2 = new Point(1428,419);
 		bdResto2 = new Point(1596,656);
-		resto2 = new BatimentCible("RestoItalien","Manger",0,50,11,23,hgResto2,bdResto2,300,1408,520);
+		resto2 = new BatimentCible("RestoItalien","Faim",0,50,11,23,hgResto2,bdResto2,300,1408,520);
 		batiments.add(resto2);
 		hgCinema = new Point(0,222);
 		bdCinema = new Point(336,449);
-		cinema = new BatimentCible("Cinema","Detente",0,80,10,22,hgCinema,bdCinema,200,336,310);
+		cinema = new BatimentCible("Cinema","Divertissement",0,80,10,22,hgCinema,bdCinema,500,336,310);
 		batiments.add(cinema);
 		hgBar1 = new Point(593,222);
 		bdBar1 = new Point(761,370);
-		bar1 = new BatimentCible("Bar","Social",0,60,12,23,hgBar1,bdBar1,230,660,202);
+		bar1 = new BatimentCible("Bar","Social",0,60,12,23,hgBar1,bdBar1,500,660,202);
 		batiments.add(bar1);
 		hgParc = new Point(1260,0);
 		bdParc = new Point(1596,409);
@@ -146,23 +150,23 @@ public class PlanVille {
 		batiments.add(parc);
 		hgBureau1 = new Point(756,0);
 		bdBureau1 = new Point(1176,148);
-		bureau1 = new BatimentCible("Bureau1","Travail",0,40,7,17,hgBureau1,bdBureau1,180,885,148);
+		bureau1 = new BatimentCible("Bureau1","Social",0,40,7,17,hgBureau1,bdBureau1,180,885,148);
 		batiments.add(bureau1);
 		hgBureau2 = new Point(415,222);
 		bdBureau2 = new Point(583,370);
-		bureau2 = new BatimentCible("Bureau2","Travail",0,20,7,17,hgBureau2,bdBureau2,180,500,202);
+		bureau2 = new BatimentCible("Bureau2","Social",0,20,7,17,hgBureau2,bdBureau2,180,500,202);
 		batiments.add(bureau2);
 		hgFFood1 = new Point(835,222);
 		bdFFood1 = new Point(919,370);
-		fastFood1 = new BatimentCible("FastFood1","Manger",0,39,11,23,hgFFood1,bdFFood1,240,815,300);
+		fastFood1 = new BatimentCible("FastFood1","Faim",0,39,11,23,hgFFood1,bdFFood1,240,815,300);
 		batiments.add(fastFood1);
 		hgFFood2 = new Point(247,523);
 		bdFFood2 = new Point(331,602);
-		fastFood2 = new BatimentCible("FastFood2","Manger",0,25,11,23,hgFFood2,bdFFood2,240,227,530);
+		fastFood2 = new BatimentCible("FastFood2","Faim",0,25,11,23,hgFFood2,bdFFood2,240,227,530);
 		batiments.add(fastFood2);
 		hgHotel = new Point(672,612);
 		bdHotel = new Point(1092,893);
-		hotel = new BatimentCible("Hotel","Habitation",0,80,5,23,hgHotel,bdHotel,310,1000,598);
+		hotel = new BatimentCible("Hotel","Energie",0,80,5,23,hgHotel,bdHotel,310,1000,598);
 		batiments.add(hotel);
 		hgBar2 = new Point(1186,489);
 		bdBar2 = new Point(1357,647);
@@ -170,28 +174,41 @@ public class PlanVille {
 		batiments.add(bar2);
 		hgDisco = new Point(929,222);
 		bdDisco = new Point(1181,370);
-		disco = new BatimentCible("Discotheque","Detente",1,68,5,23,hgDisco,bdDisco,350,1160,202);
+		disco = new BatimentCible("Discotheque","Divertissement",1,68,5,23,hgDisco,bdDisco,350,1160,202);
 		batiments.add(disco);
 		hgMairie = new Point(835,380);
 		bdMairie = new Point(1176,528);
-		mairie = new BatimentCible("Mairie","Travail",1,50,5,23,hgMairie,bdMairie,350,970,528);
+		mairie = new BatimentCible("Mairie","Social",1,50,5,23,hgMairie,bdMairie,350,970,528);
 		batiments.add(mairie);
 		hgVilla = new Point(0,0);
 		bdVilla = new Point(252,148);
-		villa = new BatimentCible("Villa","Habitation",1,90,5,23,hgVilla,bdVilla,350,126,148);
+		villa = new BatimentCible("Villa","Energie",1,90,5,23,hgVilla,bdVilla,350,126,148);
 		batiments.add(villa);
 		hgTheatre = new Point(1176,735);
 		bdTheatre = new Point(1596,893);
-		theatre = new BatimentCible("Theatre","Detente",1,70,5,23,hgTheatre,bdTheatre,350,1300,715);
+		theatre = new BatimentCible("Theatre","Divertissement",1,70,5,23,hgTheatre,bdTheatre,350,1300,715);
 		batiments.add(theatre);
 		hgParc2 = new Point(415,380);
 		bdParc2 = new Point(583,528);
-		parc2 = new BatimentCible("Statut","Detente",1,70,5,23,hgParc2,bdParc2,350,395,450);
+		parc2 = new BatimentCible("Statut","Divertissement",1,70,5,23,hgParc2,bdParc2,350,395,450);
 		batiments.add(parc2);
 		
 		
 	}
-		
+	public BatimentCible searchBat(String name) throws NoSuchElementException {
+		BatimentCible result = null;
+		for (BatimentCible b : batiments) {
+			if (b.getNom().equals(name)) {
+				result = b;
+			}
+		}
+		if (result == null) {
+			throw new NoSuchElementException("Bat does not exists.");
+		} else {
+			return result;
+		}
+	}
+
 
 
 	
@@ -828,5 +845,12 @@ public class PlanVille {
 			return false;
 		}
 	}
-
+	public BatimentCible searchByName(String name) {
+        for (int index = 0; index < batiments.size(); index++) {
+            if (batiments.get(index).getNom().equals(name)) {
+                return batiments.get(index);
+            }
+        }
+        return null;
+    }
 }
