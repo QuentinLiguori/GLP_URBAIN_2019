d6f5adee1cbe97f5484b8df7f021ad13f6981064
msabia <47143643+msabia@users.noreply.github.com>
2019-04-22 22:15:06 +0200

    Add files via upload

--- GLP_URBAIN/GLP_URBAIN_2019/src/perso/Action.java
+++ GLP_URBAIN/GLP_URBAIN_2019/src/perso/Action.java
@@ -12,13 +12,10 @@ public class Action {
 		
 	}
 	public void manger(BatimentCible bat, Personnage perso) {
-		System.out.println(bat.getPosXporte());
-		System.out.println(perso.getBesoin().getFaim());
 		if(bat.getPosXporte() == perso.getPositionX() && bat.getPosYporte() == perso.getPositionY()) {
 			Besoin bes = perso.getBesoin();
 			bes.setFaim(bes.getFaim()+bat.getValeur()/**"temps"*/);
 			perso.setBesoin(bes);	
-			System.out.println(perso.getBesoin().getFaim());
 		}
 	}
 	public void dormir(BatimentCible bat, Personnage perso) {
@@ -45,8 +42,6 @@ public class Action {
 	public void deplacer(BatimentCible bat, Personnage perso) {
 		perso.setPositionX(bat.getPosXporte());
 		perso.setPositionY(bat.getPosYporte());
-		System.out.println(perso.getPositionX());
-		System.out.println(perso.getPositionY());
 	}
 	public String toString() {
 		return "Action [type=" + type + "]";
