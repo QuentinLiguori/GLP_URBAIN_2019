b9913a06fb1f97813296bfdd1ec02eb084a4684d
llunda <47144613+llunda@users.noreply.github.com>
2019-04-24 11:53:50 +0200

    Add files via upload

--- GLP_URBAIN/GLP_URBAIN_2019/src/ihm/PersoCreation.java
+++ GLP_URBAIN/GLP_URBAIN_2019/src/ihm/PersoCreation.java
@@ -1,17 +1,26 @@
 package ihm;
 
+
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
+import javax.imageio.ImageIO;
 import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JList;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -21,136 +30,149 @@ import world.World;
 
 public class PersoCreation implements ActionListener{
 	
-	private JButton creer;
-	private JButton aleatoire;
-	private JButton jouer;
-	private JLabel creaPerso;
-	private JLabel nom;
-	private JLabel prenom;
-	private JLabel age;
-	private JLabel sexe;	
-	private JTextField newnom;
-	private JTextField newprenom;
-	private JTextField newage;
-	private JComboBox<String> newsexe;
-	private JPanel persCreationPane;
-	private JPanel topLeft;
-	private JPanel topRight;
-	private JPanel bottomLeft;
-	private JPanel bottomRight;
-	private JPanel main;
-	private World world;
+	//..................Les labels......................................
+		private JLabel persoCrea;
+		private JLabel nom;
+		private JLabel prenom;
+		private JLabel age;
+		private JLabel sexe;
+		private JLabel jeux;
+		
+	//.............Les Panels...........................................
+		private JPanel persCreationPane;
+		private JPanel left;
+		private JPanel right;
+		private JPanel topRight;
+		private JPanel bottomRight;
+		private JPanel main;
+		
+	//................Les textFields et boutons.........................
+		private JButton creer;
+		private JButton aleatoire;
+		private JButton jouer;
+		private JTextField newnom;
+		private JTextField newprenom;
+		private JTextField newage;
+		private JComboBox<String> newsexe;
+		private JList<String> list;
+		private DefaultListModel<String> mod;
+		private World world;
 	
 	public PersoCreation() {
 		
 		world = new World();
 		
-		// Initialisation de la police //
-		
-				Font font = new Font("Arial",Font.BOLD,30);
-				Font font2 = new Font("Ubuntu", Font.BOLD, 20);
-		// Initialisation du panel principal //
-		
+	//...Initialisation de la police........
+		Font font = new Font("Arial",Font.BOLD,40);
+		Font font2 = new Font("Ubuntu", Font.BOLD, 20);
+		Font font3 = new Font("Ubuntu", Font.BOLD, 30);
+	//.....Initialisation du panel............
 		persCreationPane = new JPanel();
-		persCreationPane.setLayout(new GridLayout(2, 2));
-		creaPerso = new JLabel();
-		creaPerso.setText("Création d'un personnage");
-		creaPerso.setFont(font);
+		persCreationPane.setLayout(new GridLayout(1, 2));
+		persoCrea = new JLabel("Cr�ation d'un personnage");
+		persoCrea.setFont(font);
 		main = new JPanel();
-		
-		
-		
-		
-		// Initialisation of the top left panel //
-		
-		nom = new JLabel();
-		nom.setText("Nom");
-		nom.setFont(font2);
-		newnom = new JTextField();
-		
-		prenom = new JLabel();
-		prenom.setText("Prenom");
-		prenom.setFont(font2);
-		newprenom = new JTextField();
-		
-		topLeft = new JPanel();
-		topLeft.add(nom);
-		topLeft.add(newnom);
-		topLeft.add(prenom);
-		topLeft.add(newprenom);
-		topLeft.setLayout(new BoxLayout(topLeft, 1));
-		persCreationPane.add(topLeft);
-		
-		// Initialisation of the top right panel //
-		
+	//...Inistialisation de Left Panel..........
 		sexe = new JLabel();
 		sexe.setText("Sexe");
 		sexe.setFont(font2);
 		newsexe = new JComboBox<>();
 		newsexe.addItem("Masculin");
 		newsexe.addItem("Feminin");
-		
+				
+		nom = new JLabel();
+		nom.setText("Nom");
+		nom.setFont(font2);
+		newnom = new JTextField();
+				
+		prenom = new JLabel();
+		prenom.setText("Prenom");
+		prenom.setFont(font2);
+		newprenom = new JTextField();
+				
 		age = new JLabel();
 		age.setText("Age");
 		age.setFont(font2);
 		newage = new JTextField();
-		
+				
+		left = new JPanel();
+		left.add(sexe);
+		left.add(newsexe);
+		left.add(nom);
+		left.add(newnom);
+		left.add(prenom);
+		left.add(newprenom);
+		left.add(age);
+		left.add(newage);
+		left.setLayout(new BoxLayout(left, 1));
+		persCreationPane.add(left);
+	//...Initialisation de topRight Panel....
+		list = new JList<>();
+		list.setFont(font3);
+		mod  = new DefaultListModel<>();
 		topRight = new JPanel();
-		topRight.add(sexe);
-		topRight.add(newsexe);
-		topRight.add(age);
-		topRight.add(newage);
-		topRight.setLayout(new BoxLayout(topRight, 1));
-		persCreationPane.add(topRight);
-		
-		// Initialisation des Bouttons //
-		
-		creer = new JButton();
-		creer.setText("Créer");
+		topRight.add(list);
+	//...Initiamlisation de bottomRight.....
+		creer = new JButton("Creer");
 		creer.addActionListener(this);
-		
-		bottomLeft = new JPanel();
-		bottomLeft.add(creer);
-		persCreationPane.add(bottomLeft);
-		
-		aleatoire = new JButton();
-		aleatoire.setText("Créer un personnage aléatoirement");
+		aleatoire = new JButton("Creer personnage al�atoirment");
 		aleatoire.addActionListener(this);
-		
 		bottomRight = new JPanel();
+		bottomRight.add(creer);
 		bottomRight.add(aleatoire);
-		persCreationPane.add(bottomRight);
-		
-		
-		// Initialisation du panel principal et de son boxLayout //
-		
-		jouer = new JButton();
-		jouer.setText("Jouer");
-		
-		main.add(creaPerso);
-		main.add(persCreationPane);		
+		bottomRight.setLayout(new FlowLayout(FlowLayout.CENTER,50,50));
+		creer.setPreferredSize(new Dimension(87, 53));
+		aleatoire.setPreferredSize(new Dimension(250, 53));
+	//....Iniitialisation right..........
+		right = new JPanel();
+		right.add(topRight);
+		right.add(bottomRight);
+		right.setLayout(new BoxLayout(right, 1));
+				
+		persCreationPane.add(right);
+	//....Initialisation panel priincipale et son layout...
+		jouer = new JButton("Commencer la partie");
+				
+		main.add(persoCrea);
+		main.add(persCreationPane);
 		main.add(jouer);
 		main.setLayout(new BoxLayout(main, 1));
-		
-		
-		
-		// Initialisation du JComboBox //
-		
+		jouer.setPreferredSize(new Dimension(87, 70));
+				
+	//...Initialisation du JComboBox......
 		newsexe = new JComboBox<>();
 		newsexe.addItem("Masculin");
 		newsexe.addItem("Feminin");
 		newsexe.setSelectedIndex(0);
+	}		
 	
-		
+	public void ajout() {
+		String info;
+		info = newprenom.getText()+" "+newnom.getText()+", "+newage.getText()+" ans";
+		mod.addElement(info);
+		list.setModel(mod);
 	}
 	
+	public void ajout_aleatoire(String nom,String prenom,int age) {
+		String info = prenom+" "+nom+", "+age+" ans";
+		mod.addElement(info);
+		list.setModel(mod);
+	}
+	public JButton getJouer() {
+		return jouer;
+	}
+
+	public void setJouer(JButton jouer) {
+		this.jouer = jouer;
+	}
+
 	public void actionPerformed(ActionEvent e) {
 		
 		if(e.getSource() == creer) {
 			
 			if(world.getAllCitizens().size() <5) {
-				
-				Personnage perso = new Personnage(newnom.getText(), newprenom.getText(), newsexe.getSelectedItem().toString(), 45);
+				ajout();
+				Personnage perso = new Personnage(newnom.getText(), newprenom.getText(), newsexe.getSelectedItem().toString(), 45,146,167, 159, 156);
 				world.getAllCitizens().add(perso);
 			}
 			else {
@@ -161,8 +183,12 @@ public class PersoCreation implements ActionListener{
 		if(e.getSource() == aleatoire) {
 			
 			if(world.getAllCitizens().size() <5) {
-				
-				Personnage perso = new Personnage("Gerard", "Dupond", "Masculin", 45);
+				String fname = "Jack";
+				String name = "Peralta";
+				String sex = "Masculin";
+			    int age = 30;
+			    ajout_aleatoire(name, fname, age);
+				Personnage perso = new Personnage(fname, name, sex, age,146,167, 159, 156);
 				world.getAllCitizens().add(perso);
 			}
 		}
@@ -195,7 +221,9 @@ public class PersoCreation implements ActionListener{
 	public void setPersCreationPane(JPanel persCreationPane) {
 		this.persCreationPane = persCreationPane;
 	}
-
 	
 	
 }
+
+	
+
