e8a74e92ed8a6de87855c5d998e4c588582a392e
QuentinLiguori <38524351+QuentinLiguori@users.noreply.github.com>
2019-04-26 01:40:32 +0200

    Add files via upload

--- GLP_URBAIN/GLP_URBAIN_2019/src/world/World.java
+++ GLP_URBAIN/GLP_URBAIN_2019/src/world/World.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 
 import perso.Personnage;
 
+/**
+ * Contains all the citizens in the city
+ * @author Quentin Liguori
+ *
+ */
 public class World {
 	
 	private ArrayList<Personnage> allCitizens;
@@ -12,10 +17,20 @@ public class World {
 		
 		return allCitizens;
 	}
+	
+	/**
+	 * Constructor of World, initialize the ArrayList who contain all the characters
+	 */
 	public World() {
 		
 		allCitizens = new ArrayList<>();
 	}
+	
+	/**
+	 * Return the search Citizens in the ArrayList
+	 * @param index
+	 * @return Personnage
+	 */
 	public Personnage searchCitizen(int index) {
 		
 		return getAllCitizens().get(index);
