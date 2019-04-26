d93b3f77991369c965d665555968360eb66fbd6e
msabia <47143643+msabia@users.noreply.github.com>
2019-04-26 16:40:05 +0200

    Add files via upload

--- GLP_URBAIN/GLP_URBAIN_2019/src/ihm/TopBar.java
+++ GLP_URBAIN/GLP_URBAIN_2019/src/ihm/TopBar.java
@@ -1,17 +1,21 @@
 package ihm;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import chrono.Chronometer;
 import chrono.CyclicCounter;
-
 /**
  * Class who initialize the top part of the graphic interface of the game 
  * 
  * @author Quentin Liguori
  *
  */
+
 public class TopBar extends Thread{
 	
 	public JPanel topBarPane = new JPanel();	
@@ -23,15 +27,16 @@ public class TopBar extends Thread{
     public JLabel minuteValue = new JLabel("00");
     public JLabel secondValue = new JLabel("00");
     
+    public JButton acceleratetime = new JButton("Accelerate");
+    public JButton deceleratetime = new JButton("Decelerate");
+    
     public Chronometer chronometer = new Chronometer();
-    public static final int CHRONO_SPEED = 1000;
     public boolean stop = false;
 	public String[] listday = {"Monday", "Thursday", "Wednesday", "Tuesday", "Friday", "Saturday", "Sunday"};
 	
 	public JPanel getTopBarPane() {		
 		return topBarPane;
 	}
-	
 	/**
 	 * Constructor of TopBar
 	 * 
@@ -46,9 +51,13 @@ public class TopBar extends Thread{
 	    topBarPane.add(minuteValue);
 
 	    topBarPane.add(pointLabel2);
-	    topBarPane.add(secondValue);		
+	    topBarPane.add(secondValue);
+	    
+	    deceleratetime.addActionListener(new Decelerate());
+	    topBarPane.add(deceleratetime);
+	    acceleratetime.addActionListener(new Accelerate());
+	    topBarPane.add(acceleratetime);
 	}
-	
 	/**
 	 * Update the value of the chronometer in the graphic interface
 	 * 
@@ -71,7 +80,6 @@ public class TopBar extends Thread{
         CyclicCounter second = chronometer.getSecond();
         secondValue.setText(second.toString() + "");
     }
-	
 	/**
 	 * Run the clock
 	 * 
@@ -79,17 +87,83 @@ public class TopBar extends Thread{
 	 * @see Chronometer#increment()
 	 * @see TopBar#updateValues()
 	 */
-	public void run() {
-		while (!stop) {
-			try {
-				Thread.sleep(SimuPara.SIMULATION_SPEED);
-	        } catch (InterruptedException e) {
-	        	System.out.println(e.getMessage());
+	 @Override
+	    public void run() {
+	        while (!stop) {
+	        	if(MainWindow.gameStart == true) {
+	        		try {
+	                	Thread.sleep(SimuPara.SIMULATION_SPEED);
+	            	} catch (InterruptedException e) {
+	                	System.out.println(e.getMessage());
+	            	}
+	            	if(!stop) {
+	            		chronometer.increment();
+	            		updateValues();
+	            	}
+	        	}
+	        	else {
+	        		updateValues();
+	        	}
 	        }
-	        if(!stop) {
-	        	chronometer.increment();
-	        	updateValues();
-	        }
+	    }
+	 	/**
+		 * Decelerate the game speed
+		 * 3 speeds are available 
+		 * 
+		 * @see Thread#sleep(long)
+		 * @see Chronometer#increment()
+		 * @see TopBar#updateValues()
+		 */
+		private class Decelerate implements ActionListener {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(SimuPara.SIMULATION_SPEED == 1) {
+					SimuPara.SIMULATION_SPEED = 10;
+					deceleratetime.setText("Decelerate x10");
+					acceleratetime.setText("Accelerate");
+				}
+				else if(SimuPara.SIMULATION_SPEED == 10) {
+					SimuPara.SIMULATION_SPEED = 100;
+					deceleratetime.setText("Decelerate x100");
+					acceleratetime.setText("Accelerate");
+				}
+				else if(SimuPara.SIMULATION_SPEED == 100) {
+					SimuPara.SIMULATION_SPEED = 1000;
+					deceleratetime.setText("Decelerate");
+					acceleratetime.setText("Accelerate");
+				}
+			}
+
 		}
-	}
+		/**
+		 * Accelerate the game speed
+		 * 3 speeds are available 
+		 * 
+		 * @see Thread#sleep(long)
+		 * @see Chronometer#increment()
+		 * @see TopBar#updateValues()
+		 */
+		private class Accelerate implements ActionListener {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(SimuPara.SIMULATION_SPEED == 1000) {
+					SimuPara.SIMULATION_SPEED = 100;
+					acceleratetime.setText("Accelerate x10");
+					deceleratetime.setText("Decelerate");
+				}
+				else if(SimuPara.SIMULATION_SPEED == 100) {
+					SimuPara.SIMULATION_SPEED = 10;
+					acceleratetime.setText("Accelerate x100");
+					deceleratetime.setText("Decelerate");
+				}
+				else if(SimuPara.SIMULATION_SPEED == 10) {
+					SimuPara.SIMULATION_SPEED = 1;
+					acceleratetime.setText("Accelerate MAX");
+					deceleratetime.setText("Decelerate");
+				}				
+			}
+
+		}
 }
